8393e1bc3b628c4888e80f59fc8a76fcf9c00d58
Niels-1068212 <143613483+Niels-1068212@users.noreply.github.com>
2024-09-04 17:25:40 +0200

    counter working

--- main.js
+++ main.js
@@ -1,57 +1,17 @@
 window.addEventListener("load", () => {
     clock();
     function clock() {
-      const today = new Date();
-  
-      // get time components
-      const hours = today.getHours();
-      const minutes = today.getMinutes();
-      const seconds = today.getSeconds();
-  
-      //add '0' to hour, minute & second when they are less 10
-      const hour = hours < 10 ? "0" + hours : hours;
-      const minute = minutes < 10 ? "0" + minutes : minutes;
-      const second = seconds < 10 ? "0" + seconds : seconds;
-  
-      //make clock a 12-hour time clock
-      const hourTime = hour > 12 ? hour - 12 : hour;
-  
-      // if (hour === 0) {
-      //   hour = 12;
-      // }
-      //assigning 'am' or 'pm' to indicate time of the day
-      const ampm = hour < 12 ? "AM" : "PM";
-  
-      // get date components
-      const month = today.getMonth();
-      const year = today.getFullYear();
-      const day = today.getDate();
-  
-      //declaring a list of all months in  a year
-      const monthList = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December"
-      ];
-  
-      //get current date and time
-      const date = monthList[month] + " " + day + ", " + year;
-      const time = hourTime + ":" + minute + ":" + second + ampm;
-  
-      //combine current date and time
-      const dateTime = date + " - " + time;
+        var a = new Date(2003, 9, 10); // 29 januari 2007
+        var b = new Date(); // Huidige datum en tijd
+
+        // Bereken het verschil in milliseconden
+        var differenceInMillis = b - a;
+
+        // Zet het verschil om naar dagen
+        var differenceInDays = Math.floor(differenceInMillis / (1000 * 60 * 60 * 24));
   
       //print current date and time to the DOM
-      document.getElementById("date-time").innerHTML = dateTime;
+      document.getElementById("date-time").innerHTML = "Al" + " " + differenceInDays + " " + "dagen getrouwd";
       setTimeout(clock, 1000);
     }
   });
